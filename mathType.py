f4e1d55206b2b353457e9411bd1f67c80223f1d5
lancelot <lancelot.pro@outlook.com>
2017-05-16 10:26:36 +0800

    type..if ...while...for

---
+++ mathType.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3.5
+
+a = "Hello";
+
+b = "Python";
+
+print("输出结果" + a + b);
+
+
+#条件输出
+
+if("HH" in a):
+	print("in th a!");
+else:
+	print("not in a!");
+
+	
+print("#############");
+#circle
+
+a = 10;
+b = 1;
+
+while b < a:
+	print(b);
+	b = b + 1;
+print("#############");
+lista = [1,2,3,4,5];
+a = 1;
+
+for a in lista:
+	print(a);
